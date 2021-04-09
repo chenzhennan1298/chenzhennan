7f939396bd93c8ac60ba5178bd0f7d121730fbf3
chenzhennan1298 <18481072477@163.com>
2021-04-09 11:11:41 +0800

    修改脚本

--- jenkinspytest/api/login/sign_in.py
+++ jenkinspytest/api/login/sign_in.py
@@ -4,7 +4,7 @@ from api.base_api import BaseApi
 
 class Signin(BaseApi):
 
-    api_path = "data\login\login.yml"
+    api_path = "data/login/login.yml"
     def signin(self,username ,password):
         # Template模板二次修改的值，p_data
         p_data = {"ip":self.ip,"username":username,"password":password}
